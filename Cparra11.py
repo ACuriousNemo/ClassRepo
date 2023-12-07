4e4ad7887e4e76fd966a414cc0c631f83ccc1d61
ACuriousNemo <parritos97@yahoo.com>
2023-12-07 18:02:08 +0000

    "This is for Lab 11"

---
+++ Cparra11.py
@@ -0,0 +1,5 @@
+#Program created by Carlos Parra
+
+userInput = input("What number do you want squared?\n")
+print("Your number squared is ", float(userInput)**2, "\n")
+print("A man's face is his autobiagraphy. A woman's face is a work of fiction. -Oscar Wilde")
